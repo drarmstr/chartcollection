7427fb0185a3b0a9587695f5853f124f4a66b75f
Douglas Armstrong <dougarmstrong@fb.com>
2016-09-14 10:53:31 -0700

    Temporary hack to publish NPM module

--- js/c3.js
+++ js/c3.js
@@ -21,7 +21,8 @@
   })();
 
   this.c3 = c3;
-  module.exports = c3;
+  // TODO: Temporary hack to export NPM module
+  if (module) module.exports = c3;
 
   if (typeof d3 === "undefined" || d3 === null) {
     throw Error("D3 library is required for C3");
